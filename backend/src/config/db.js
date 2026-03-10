17c312f5423108220db02fcbaacd906a5b545119
NJI HALLE <njihalle10@gmail.com>
2026-03-10 21:14:27 +0100

    modified:   backend/src/config/db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-<<<<<<< HEAD
         const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
-=======
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`)
->>>>>>> b7785cd1b691b95f8c258e5604028ee49a0a95f9
         console.log(`\n MongDB connected successfully: ${connectionInstance.connection.host} \n`);
     } catch (error){
         console.log(`MongoDB connection failed: ${error.message}`);
